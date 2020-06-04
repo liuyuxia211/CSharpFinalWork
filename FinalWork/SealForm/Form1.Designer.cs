6f93ec7d96efe2ce7194021dc6f028a81fe46245
yuxia liu <1138310046@qq.com>
2020-06-04 11:17:01 +0800

    mfinal work

---
+++ FinalWork/SealForm/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace SealForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 492);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1035, 94);
+            this.panel1.TabIndex = 3;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(785, 36);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "drawout";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(675, 36);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "insert";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(583, 36);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "crop";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(452, 36);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "water";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(462, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(359, 36);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(72, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "fun2";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(258, 33);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "fun1";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(160, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(56, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1035, 60);
+            this.panel2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(749, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "效果图";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "原图像";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(631, 91);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(392, 337);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(56, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(450, 420);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 586);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ FinalWork/SealForm/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace SealForm
+{
+
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap;
+        Bitmap previousMap = null;
+        ImageEffectManager imageManager;
+        ImageEventHandlers imageHandlers;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage ; //这里用来设置如何处理图片
+
+            imageHandlers = new ImageEventHandlers();
+            
+            imageManager = imageHandlers.ImageManager;//将handler中的manager赋值给form中的manager
+            imageHandlers.PictureBox1 = pictureBox1;//将form中的picturebox赋值给handler中picturebox，不然为空
+            imageHandlers.TimeLabel = label1;
+           imageHandlers.TimeLabel.DataBindings.Add("Text", imageHandlers,"TimeElapsed");
+            //imageHandlers.TimeLabel.Text = "";
+
+            //各种事件
+            pictureBox1.AllowDrop = true;
+            //pictureBox2.AllowDrop = true;
+            //图片事件
+            pictureBox1.MouseWheel += imageHandlers.pic1_MouseWheel;
+            pictureBox1.MouseMove += imageHandlers.pictureBox1_MouseMove;
+            pictureBox1.MouseDown += imageHandlers.pictureBox1_MouseDown;
+            pictureBox1.MouseUp += imageHandlers.pictureBox1_MouseUp;
+            pictureBox1.DragEnter += imageHandlers.pictureBox1_DragEnterLoad;
+            pictureBox1.DragDrop += imageHandlers.pictureBox1_DragDropLoad;
+            //MouseWheel += new MouseEventHandler();
+            //按钮事件
+            button1.Click += imageHandlers.loadImage_Click;
+            button2.Click += imageHandlers.saveImage_Click;
+            button5.Click += imageHandlers.waterPic_Click;
+            button6.Click += imageHandlers.cropPic_Click;
+            button7.Click += imageHandlers.insertPic_Click;
+            button8.Click += imageHandlers.drawOut_Click;
+
+
+
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            /*
+                TimeCounter.Start();
+                //string path = openFileDialog1.FileName;
+                Image image = (Bitmap)Image.FromFile(@"C:\Users\asus\Desktop\Gavin.jpg");
+                bitmap = new Bitmap(image, pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.Image = bitmap.Clone() as Image;
+                imageManager.oldBitmap = bitmap.Clone() as Bitmap;
+                TimeCounter.Stop();
+                label1.Text = TimeCounter.Seconds.ToString();
+                */
+           // }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int width = pictureBox1.Width / 2;
+            int height = pictureBox1.Height / 2;
+            Image image;
+            image = imageManager.resizeImage(pictureBox1.Image,width,height);
+            pictureBox1.Image = image;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*
+            TimeCounter.Start();
+            imageManager.ChangeEffect(ImageEffect.GrayScale);
+            pictureBox2.Image = imageManager.newBitmap.Clone() as Image;
+            TimeCounter.Stop();
+            label1.Text = TimeCounter.Seconds.ToString();
+            */
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            /*
+            TimeCounter.Start();
+            imageManager.ChangeEffect(ImageEffect.Sharpen);
+            pictureBox2.Image = imageManager.newBitmap.Clone() as Image;
+            TimeCounter.Stop();
+            label1.Text = TimeCounter.Seconds.ToString();
+            */
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            PictureBox pictureBox = sender as PictureBox;
+            
+
+            if (pictureBox != null && e.Button == MouseButtons.Left)
+            {
+                Bitmap mapData = pictureBox.Image as Bitmap;
+                DoDragDrop(mapData, DragDropEffects.Copy);
+
+            }
+        }
+
+        private void pictureBox2_DragEnter(object sender, DragEventArgs e)
+        {
+            Console.WriteLine(e.AllowedEffect);
+            PictureBox pictureBox = sender as PictureBox;
+            if (pictureBox != null)
+            {
+                // Save the current Fill brush so that you can revert back to this value in DragLeave.
+                previousMap = pictureBox.Image.Clone() as Bitmap;
+
+                // If the DataObject contains string data, extract it.
+                if (e.Data.GetDataPresent(DataFormats.Bitmap))
+                {
+                    Bitmap dataMap = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+
+                    // If the string can be converted into a Brush, convert it.
+
+
+                    if (dataMap != null)
+                    {
+                        e.Effect = e.AllowedEffect;
+                        pictureBox.Image = dataMap;
+
+                    }
+                }
+                else if (e.Data.GetDataPresent(DataFormats.Text))
+                {
+                    string effect = e.Data.GetData(DataFormats.Text).ToString();
+                    Console.WriteLine(effect);
+                    switch (effect)
+                    {
+                        case "fun1":
+                            //imageManager.ChangeEffect(ImageEffect.GrayScale);
+                            //pictureBox.Image = imageManager.newBitmap.Clone() as Image;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+            }
+
+
+        }
+        private void pictureBox2_DragLeave(object sender, EventArgs e)
+        {
+            //Bitmap map = sender as Bitmap;
+            PictureBox pictureBox = sender as PictureBox;
+            if (pictureBox != null)
+            {
+                pictureBox.Image = previousMap;
+            }
+            
+        }
+
+        private void pictureBox2_DragDrop(object sender, DragEventArgs e)
+        {
+
+            PictureBox pictureBox = sender as PictureBox;
+            if (pictureBox != null)
+            {
+
+
+                // If the DataObject contains string data, extract it.
+                if (e.Data.GetDataPresent(DataFormats.Bitmap))
+                {
+                    Bitmap dataMap = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+
+                    // If the string can be converted into a Brush, convert it.
+
+
+                    if (dataMap != null)
+                    {
+                        // e.Effect = e.AllowedEffect;
+                        pictureBox.Image = dataMap;
+
+                    }
+                }
+            }
+        }
+
+
+
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        private void pictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("hello");
+            //string path = e.Data.GetData(DataFormats.FileDrop).ToString();
+            string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            try
+            {
+                Image image = Image.FromFile(path);
+                pictureBox1.Image = new Bitmap(image, pictureBox1.Width, pictureBox1.Height);
+                imageManager.oldBitmap = image.Clone() as Bitmap;
+            }
+            catch (Exception E)
+            {
+
+                Console.WriteLine(E.Message);
+            }
+
+
+            Console.WriteLine(path);
+        }
+
+        private void button3_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button != null && e.Button == MouseButtons.Left)
+            {
+                DoDragDrop(button.Text, DragDropEffects.Copy);
+            }
+        }
+
+        private void button4_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Image image;
+            image = imageManager.MakeWaterMark(pictureBox1.Image);
+            pictureBox2.Image = image;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Image image;
+           //image =  imageManager.DrawOutCropArea(0, 0, 200, 200);
+           // pictureBox2.Image = image;
+            //pictureBox1.Image = imageManager.Crop(0, 0, 200, 200);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+           // imageManager.oldBitmap = pictureBox2.Image as Bitmap;
+            pictureBox2.Image = imageManager.InsertImage(pictureBox1.Image,0,0);
+        }
+
+
+    }
+
+
+}
+
+
+
